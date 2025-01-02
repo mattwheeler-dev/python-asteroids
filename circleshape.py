6acd16784e0451ddc932d2bd047e47208fc1faf1
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2025-01-01 18:05:06 -0800

    create asteroids, shots, and field; handle all collision cases; split or delete asteroids based on size;

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,8 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision_check(self, other):
+        distance =  pygame.math.Vector2.distance_to(self.position, other.position)
+        if self.radius + other.radius > distance:
+            return True
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()    
@@ -10,9 +13,16 @@ def main():
     # groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Shot.containers = (updatable, drawable, shots)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     dt = 0
 
     while True:
@@ -22,6 +32,15 @@ def main():
 
         for item in updatable:
             item.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision_check(player) == True:
+                print("Game Over!")
+                exit()
+            for bullet in shots:
+                if asteroid.collision_check(bullet) == True:
+                    bullet.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
